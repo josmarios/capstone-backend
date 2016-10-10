4e59dd0be5c26dc4032578ebe4cfb5c169415549
Josmario Albuquerque <josmarios.silva@gmail.com>
2016-10-09 22:56:24 -0300

    initial commit

---
+++ src/java/ic/lps/tutor/CapstoneServlet.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ic.lps.tutor;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.json.JsonObject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.glassfish.json.JsonParserImpl;
+import org.json.JSONObject;
+
+/**
+ *
+ * @author josmario
+ */
+@WebServlet(name = "CapstoneServlet", urlPatterns = {"/CapstoneServlet"})
+public class CapstoneServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        response.setContentType("text/html;charset=UTF-8");
+//        try (PrintWriter out = response.getWriter()) {
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet CapstoneServlet</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Servlet CapstoneServlet at " + request.getContextPath() + "</h1>");
+//            out.println("</body>");
+//            out.println("</html>");
+//        }
+
+        JSONObject json = new JSONObject(request.getReader().readLine());
+
+        String csvHeader = "gender, age, testType, pretest, activity, posttest\n";
+        String csvLine = json.getString("gender") + "," + json.getString("age") + "," + json.getString("testType") + "," + json.getInt("pretestPoints") + "," + json.getInt("activityPoints") + "," + json.getInt("posttestPoints")+"\n";
+
+        File output = new File("/home/josmario/responses.csv");
+
+        if (output.exists()) {
+            FileWriter fw = new FileWriter(output, true);
+            fw.append(csvLine);
+            fw.close();
+        } else {
+            FileWriter fw = new FileWriter(output, true);
+            fw.write(csvHeader + csvLine);
+            fw.close();
+        }
+
+        System.out.println(csvHeader + csvLine);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
